452ec833ee309eb7f6d016c2353f50d1ecb3938c
Paul Elliott <paulelliott@pop-os.localdomain>
2022-01-06 14:31:27 -0700

    dashboard beginning

--- client/src/screens/Dashboard/Dashboard.tsx
+++ client/src/screens/Dashboard/Dashboard.tsx
@@ -6,7 +6,10 @@ import PlaceholderParagraph from "rsuite/esm/Placeholder/PlaceholderParagraph";
 import Avatar from "react-avatar";
 import { IoMan } from "react-icons/io5";
 import { FaCat, FaDog } from "react-icons/fa";
-
+import Keeley from "./Keeley.jpg";
+import Inca from "./Inca.jpg";
+import Panda from "./Panda.jpg";
+import Paul from "./Paul.jpg";
 export default function Dashboard() {
   return (
     <div className={styles.app}>
@@ -22,27 +25,42 @@ export default function Dashboard() {
             <FaCat size={"50px"}></FaCat>
           </div>
         </Panel>
-        <Panel>
+        <Panel header={"The Regional Manager"}>
           <div style={{ display: "grid", gridTemplateColumns: "200px auto" }}>
-            <Avatar size="150px" name="Keeley" />
+            <Avatar size="150px" round={true} src={Paul} name="Paul" />
             <PlaceholderParagraph />
+          </div>
+        </Panel>
+        <Panel header={"Assistant to the Regional Manager"}>
+          <div style={{ display: "grid", gridTemplateColumns: "auto 200px" }}>
+            <PlaceholderParagraph />
+            <Avatar size="150px" round={true} name="Amanda" />
           </div>
         </Panel>
         <Panel>
           <div style={{ display: "grid", gridTemplateColumns: "200px auto" }}>
-            <Avatar size="150px" name="Inca" />
+            <div style={{ display: "block", textAlign: "center" }}>
+              <h6>Salesman</h6>
+              <Avatar size="150px" src={Panda} round name="Panda" />
+            </div>
             <PlaceholderParagraph />
           </div>
         </Panel>
         <Panel>
-          <div style={{ display: "grid", gridTemplateColumns: "200px auto" }}>
-            <Avatar size="150px" name="Panda" />
+          <div style={{ display: "grid", gridTemplateColumns: "auto 200px" }}>
             <PlaceholderParagraph />
+            <div style={{ display: "block", textAlign: "center" }}>
+              <h6>Salesman</h6>
+              <Avatar size="150px" src={Keeley} round name="Keeley" />
+            </div>
           </div>
         </Panel>
         <Panel>
           <div style={{ display: "grid", gridTemplateColumns: "200px auto" }}>
-            <Avatar size="150px" name="Paul" />
+            <div style={{ display: "block", textAlign: "center" }}>
+              <h6>Accountant</h6>
+              <Avatar size="150px" src={Inca} round name="Inca" />
+            </div>
             <PlaceholderParagraph />
           </div>
         </Panel>
