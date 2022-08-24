29ae219dadfe098dfab6f5a9285263eddbb74d59
wuzzlefump <paulelliottwork@gmail.com>
2022-08-24 13:13:59 -0600

    workout website added

--- src/screens/Websites/Websites.tsx
+++ src/screens/Websites/Websites.tsx
@@ -1,34 +1,66 @@
-import * as React from "react"
 import TopBar from "../../components/TopBar"
 import styles from "./Websites.module.css"
-import { useNavigate } from "react-router"
-import {FaHandsHelping} from "react-icons/fa"
+import {FaHandsHelping, FaHandLizard} from "react-icons/fa"
+import {GiWeightScale} from "react-icons/gi"
 export default function Websites(){
 
+interface webItem{
+    title:string;
+    description:string;
+    link:string;
+    icon:any;
+}
+let list :webItem[] =[
+    {
+        title:" React Dance Helper",
+        description:"A site designed to help educate people about partner dancing.",
+        link:"https://wuzzlefump.github.io/ReactDanceHelper/",
+        icon:<FaHandsHelping/>
+    },
+    {
+        title:"R-P-S-L-S",
+        description:"ROCK PAPER SCISSORS ... LIZARD? SPOCK?",
+        link:"https://wuzzlefump.github.io/wuzzlefump.github.io-rock-paper-scissors-lizard-spock/",
+        icon:<FaHandLizard/>,
+    },
+    {
+        title:"Workout Local",
+        description:"Keep track of your weight and health using the browser's local storage",
+        link:"https://wuzzlefump.github.io/workout-local/",
+        icon:<GiWeightScale/>,
+    }
+]
     return(
         <div className={styles.app}>
             <TopBar/>
+            <h3 className={styles.header}>
+                Simple Sites Paul Made
+            </h3>
             <div className={styles.container}>
-            <WebCard link="https://wuzzlefump.github.io/ReactDanceHelper/">
-                <div>
-                    <FaHandsHelping/>
-                </div>
-                <h6>
-                    React Dance Helper
-                </h6>
+                {
+                    list.map(item=>{
+                        return(
+                            <WebCard link={item.link}>
+                                <div>
+                                    {item.icon}
+                                </div>
+                                <h6>
+                                    {item.title}
+                                </h6>
                 
-                <p style={{fontSize:"15px", marginBlockStart:"10px"}}>
-                    A site designed to help educate people about partner dancing.
-                </p>
-
-            </WebCard>
+                                 <p style={{fontSize:"15px", marginBlockStart:"10px"}}>
+                                    {item.description}
+                                </p>
+                            </WebCard>
+                        )
+                    })
+                }
             </div>
         </div>
     )
 }
 
 const WebCard =(props:{children:any, link:string})=>{
-    const navigate = useNavigate()
     return(
         <div onClick={()=>window.open(props.link,"_blank")} className={styles.webcard}>
             {props.children}
